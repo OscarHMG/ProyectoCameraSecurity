74c09e1a47ba491a8b3b1829807780a521e5e9e3
unknown <labproto2@CTI_THIN_54_EN.cti.espol.edu.ec>
2015-11-30 17:44:25 -0500

    initial progress about the motion detect from camera

--- CameraSecurity/CameraSecurity/Program.cs
+++ CameraSecurity/CameraSecurity/Program.cs
@@ -11,30 +11,84 @@ using Microsoft.SPOT.Touch;
 using Gadgeteer.Networking;
 using GT = Gadgeteer;
 using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
 
 namespace CameraSecurity
 {
     public partial class Program
     {
-        // This method is run when the mainboard is powered up or reset.   
+        public Bitmap actualPicture;
+        public ArrayList pictures;
+        private static int contador = 0;
         void ProgramStarted()
         {
-            /*******************************************************************************************
-            Modules added in the Program.gadgeteer designer view are used by typing 
-            their name followed by a period, e.g.  button.  or  camera.
-            
-            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
-                button.ButtonPressed +=<tab><tab>
-            
-            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
-                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
-                timer.Tick +=<tab><tab>
-                timer.Start();
-            *******************************************************************************************/
-
-
-            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
+            camera.BitmapStreamed += camera_BitmapStreamed;
+            button.ButtonPressed += button_ButtonPressed;
+            camera.PictureCaptured += camera_PictureCaptured;
+            pictures = new ArrayList();
             Debug.Print("Program Started");
         }
+
+        void camera_PictureCaptured(Camera sender, GT.Picture picture)
+        {
+            camera.TakePicture();
+        }
+
+        void button_ButtonPressed(Button sender, Button.ButtonState state)
+        {
+            camera.StartStreaming();
+        }
+
+        void camera_BitmapStreamed(Camera sender, Bitmap bitmap)
+        {
+            displayT35.SimpleGraphics.DisplayImage(bitmap, 0, 0);
+            if (bitmap != null) {
+                pictures.Add(bitmap);
+                contador++;
+                Debug.Print("Cont: "+pictures.Capacity);
+                compareBitmaps(pictures);
+            }
+  
+        }
+
+        private void compareBitmaps(ArrayList pictures)
+        {
+            int i = 0;
+           
+            if (pictures.Count >= 2) {
+                for (i = 0; i < pictures.Count-1; i++)
+                {
+                    if (Equals((Bitmap)pictures[i], (Bitmap)pictures[i + 1]))
+                    {
+                        Debug.Print("SAME PIC");
+                    }
+                }
+            }
+        }
+
+        private static bool Equals(Bitmap bmp1, Bitmap bmp2)
+        {
+            int cont = 0;
+            for (int x = 0; x < bmp1.Width; ++x)
+            {
+                for (int y = 0; y < bmp1.Height; ++y)
+                {
+                    if (bmp1.GetPixel(x, y) != bmp2.GetPixel(x, y))
+                    {
+                        cont++;
+                    }
+                }
+            }
+            if (cont >= 50) {
+                return false;
+            }
+            return true;
+        }
+
+        void camera_CameraConnected(Camera sender)
+        {
+            Debug.Print("CAMERA IS ON");
+        }
+
     }
 }
