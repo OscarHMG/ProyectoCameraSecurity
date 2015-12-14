40667ec7ce19e453c7d87fadf2533186133603d5
unknown <labproto2@CTI_THIN_54_EN.cti.espol.edu.ec>
2015-12-14 18:23:45 -0500

    The camera is detecting the motion, check the function "sendNotication" (Libraries issues)

--- MotionCamera/GadgeteerApp1/Program.cs
+++ MotionCamera/GadgeteerApp1/Program.cs
@@ -11,8 +11,8 @@ using Gadgeteer.Networking;
 using GT = Gadgeteer;
 using GTM = Gadgeteer.Modules;
 using Gadgeteer.Modules.GHIElectronics;
-using Microsoft.SPOT.Presentation.Shapes;
-using Microsoft.SPOT.Input;
+using System.Web.Mail;
+
 namespace GadgeteerApp1
 {
     public partial class Program
@@ -26,12 +26,17 @@ namespace GadgeteerApp1
         ArrayList posiciones = new ArrayList();
         ArrayList actualColores = new ArrayList();
         ArrayList lastColores = new ArrayList();
+        static Bitmap currentBitmap, previousBitmap;
+        static Boolean block = false;
+        private GT.Timer timer;
         void ProgramStarted()
         {
             mBitmap = new Bitmap(camera.CurrentPictureResolution.Width, camera.CurrentPictureResolution.Height); //initialize buffer to camera view size
             camera.BitmapStreamed += camera_BitmapStreamed;
             button.ButtonPressed += button_ButtonPressed;
             button.TurnLedOff(); //mark button as "off"
+            timer = new GT.Timer(2000);
+            timer.Tick += new GT.Timer.TickEventHandler(timer_Tick);
             Debug.Print("Program Started");
         }
 
@@ -47,7 +52,7 @@ namespace GadgeteerApp1
                 if (camera.CameraReady)
                 {
                     camera.StartStreaming(mBitmap); //start streaming
-                    fillColores();
+                    //fillColores();
                     button.TurnLedOn(); //mark button as "on"
                 }
             }
@@ -58,39 +63,16 @@ namespace GadgeteerApp1
         void camera_BitmapStreamed(GTM.GHIElectronics.Camera sender, Bitmap bitmap)
         {
             //320*240
+
             displayT35.SimpleGraphics.DisplayImage(bitmap, 0, 0);
-            FillColor(bitmap);
-            detectionMove();
+            detectionMove(bitmap);
         }
 
 
-
-        private int detectMotion(Bitmap bitmap, int pX, int pY, Color newColor)
-        {
-            int resultado = 0;
-            newColor = bitmap.GetPixel(pX, pY);
-            var r = ColorUtility.GetRValue(newColor);
-            var g = ColorUtility.GetGValue(newColor);
-            var b = ColorUtility.GetBValue(newColor);
-
-            if (System.Math.Abs(r - mLastR) + System.Math.Abs(g - mLastG) + System.Math.Abs(b - mLastB) >= 100)
-            {
-                Debug.Print("Motion!!");
-                resultado = System.Math.Abs(r - mLastR) + System.Math.Abs(g - mLastG) + System.Math.Abs(b - mLastB);
-
-            }
-
-            mLastR = r;
-            mLastB = b;
-            mLastG = g;
-
-            return resultado;
-        }
-
-
-        void FillColor(Bitmap bitmap)
+        ArrayList FillColor(Bitmap bitmap)
         {
             int i, j;
+            ArrayList temp = new ArrayList();
             int posX = 0, posY = 0;
 
             for (i = 0; i < 3; i++)
@@ -100,59 +82,80 @@ namespace GadgeteerApp1
                     var pixel = bitmap.GetPixel(posX, posY);
                     Color color = ColorUtility.ColorFromRGB(ColorUtility.GetRValue(pixel),
                                   ColorUtility.GetGValue(pixel), ColorUtility.GetBValue(pixel));
-                    posiciones.Add(pixel);
-                    actualColores.Add(color);
-
+                    temp.Add(color);
                     posX += 159;
                 }
                 posY = +119;
                 posX = 0;
             }
-
+            return temp;
         }
 
-
-        private int detectionMove()
+        void detectionMove(Bitmap bitmap)
         {
-            int i;
-            Color color, lastColor;
-            for (i = 0; i < 9; i++)
+            int cont = 0;
+            if (currentBitmap != null)
             {
-                var posicion = posiciones[i];
-                color = (Color)actualColores[i];
-                lastColor = (Color)actualColores[i];
-                var r = ColorUtility.GetRValue(color);
-                var g = ColorUtility.GetGValue(color);
-                var b = ColorUtility.GetBValue(color);
-                var mLastR = ColorUtility.GetRValue(lastColor);
-                var mLastG = ColorUtility.GetGValue(lastColor);
-                var mLastB = ColorUtility.GetBValue(lastColor);
-                if (mFirst)
+                previousBitmap = currentBitmap;
+                lastColores = FillColor(previousBitmap);
+            }
+
+            if (previousBitmap != null)
+            {
+
+                for (int x = 0; x < 9; x++)
                 {
-                    mFirst = false;
+                    Color oldColour = (Color)lastColores[x];
+                    Color newColour = (Color)actualColores[x];
+                    int deltaRed = System.Math.Abs(ColorUtility.GetRValue(oldColour) - ColorUtility.GetRValue(newColour));
+                    int deltaGreen = System.Math.Abs(ColorUtility.GetGValue(oldColour) - ColorUtility.GetGValue(newColour));
+                    int deltaBlue = System.Math.Abs(ColorUtility.GetBValue(oldColour) - ColorUtility.GetBValue(newColour));
+                    int deltaTotal = deltaRed + deltaGreen + deltaBlue;
+                    if (deltaTotal > 50)
+                    {
+                        cont++;
+                    }
+                }
+                if (cont >= 1)
+                {
+                    block = true;
+                    timer.Start();
                 }
                 else
                 {
-                    if (System.Math.Abs(r - mLastR) + System.Math.Abs(g - mLastG) + System.Math.Abs(b - mLastB) >= 100)
-                    {
-                        Debug.Print("Motion!!");
-                        //Manda mensaje
-                        return 1; //Intrusop
-                    }
-                }//Ahora actualizo los colores pasados con los actuales para cada punto
-                Color newColor = ColorUtility.ColorFromRGB(r, g, b);
-                lastColores[i] = newColor;
+                    Debug.Print("Nothing");
+                }
             }
-            return 0;
+            currentBitmap = bitmap;
+            actualColores = FillColor(currentBitmap);
         }
 
-        void fillColores()
+
+        private void timer_Tick(GT.Timer timer)
         {
-            int i =0;
-            for (i = 0; i < 9; i++ )
-            {
-                lastColores.Add(ColorUtility.ColorFromRGB(0, 0, 0));
-            }
+           if(block){
+               sendNotification("smoncayo@espol.edu.ec","omoncayo@espol.edu.ec","Aviso, AALARMA!","Intruso detectado");
+               block = false;
+           }
         }
+        public static void sendNotification(string MessageFrom, string MessageTo, string MessageSubject, string MessageBody)
+    {
+      MailMessage message = new MailMessage();
+      message.From        = MessageFrom;
+      message.To          = MessageTo;
+      message.Subject     = MessageSubject;
+      message.BodyFormat  = MailFormat.Text;
+      message.Body        = MessageBody;
+ 
+      try
+      {
+        Debug.Print("Mensaje enviandose");
+        SmtpMail.Send(message);
+      }
+      catch( Exception exHttp )
+      {
+          Debug.Print("ERROR enviando notificacion");
+    }
+  }
     }
 }
